e6148b7d7e67b3af8d2372ade26e6770ad60fa62
Corro kun <networpava@gmail.com>
2024-04-20 23:58:23 -0500

    changer icons

--- frontend/src/components/LoginRegister/LoginRegister.jsx
+++ frontend/src/components/LoginRegister/LoginRegister.jsx
@@ -1,9 +1,35 @@
+import {checkUpdate, installUpdate, onUpdaterEvent} from "@tauri-apps/api/updater";
+import {relaunch} from "@tauri-apps/api/process";
 import "./LoginRegister.css"
+import {useEffect} from "react";
 import { useProfile } from "../../context/profile";
 import {toast} from "sonner";
 
 export default function LoginRegister(){
   const {ChangerLogin, HandleLogin} = useProfile();
+
+  async function update(){
+    const {shouldUpdate, manifest} = await checkUpdate();
+    const unlisten = await onUpdaterEvent(({error, status})=>{
+      console.log(status);
+    });
+    if(shouldUpdate){
+      toast(`Nueva actualización ${manifest?.version}`,{
+        action: {
+          label: "Actualizar",
+          onClick: async()=>{
+            await installUpdate();
+            await relaunch();
+          }
+        }
+      });
+    }
+    unlisten();
+  }
+
+  useEffect(()=>{
+    update();
+  },[]);
 
   return(
   <div className="Render-LoginRegister">
